500e17e2ab93e8dded54b6102703580ad842afa7
Vadym Zelenko <115219323+vadymzelenko@users.noreply.github.com>
2026-09-17 06:59:36 +0200

    Update TouchControlsScene.js

--- src/input/ui/TouchControlsScene.js
+++ src/input/ui/TouchControlsScene.js
@@ -1,23 +1,27 @@
-import { TouchBus } from '../TouchBus.js';
-import { VirtualJoystick } from './VirtualJoystick.js';
-import { TouchButton } from './TouchButton.js';
+import { TouchBus } from '../input/TouchBus.js';
+import { VirtualJoystick } from '../input/VirtualJoystick.js';
+import { TouchButton } from '../input/TouchButton.js';
 
 export class TouchControlsScene extends Phaser.Scene {
     constructor() { super('TouchControlsScene'); }
 
     create() {
         TouchBus.enabled = true;
+        TouchBus.reset();
 
         this.joystick = new VirtualJoystick(this);
 
-        const w = this.scale.width, h = this.scale.height;
+        const w = this.scale.width;
+        const h = this.scale.height;
 
         this.interactBtn = new TouchButton(this, w - 90, h - 100, {
-            label: 'E', radius: 44,
+            label: 'E',
+            radius: 44,
         });
 
         this.pauseBtn = new TouchButton(this, w - 34, 40, {
-            label: '⏸', radius: 24,
+            label: '⏸',
+            radius: 24,
         });
 
         this.scale.on('resize', this._layout, this);
@@ -25,12 +29,14 @@ export class TouchControlsScene extends Phaser.Scene {
 
         this.events.once('shutdown', () => {
             TouchBus.enabled = false;
+            TouchBus.reset();
             this.scale.off('resize', this._layout, this);
         });
     }
 
     _layout() {
-        const w = this.scale.width, h = this.scale.height;
+        const w = this.scale.width;
+        const h = this.scale.height;
         this.joystick.setHome(110, h - 110);
         this.interactBtn.setPosition(w - 90, h - 100);
         this.pauseBtn.setPosition(w - 34, 40);
